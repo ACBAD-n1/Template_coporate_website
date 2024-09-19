bd1407a0d6bb9f9ea1a15c6982b9e79c2ae5b3e9
Adam Boureima <adamcbad@gmail.com>
2024-09-19 21:23:01 +0100

    Make fully responsif the site and add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
+// Toggle mobile menu
+function toggleMenu() {
+    const menu = document.querySelector('.nav-menu');
+    menu.classList.toggle('active');
+}
+
 // Script To activate scrolling
 AOS.init();
